a83f9920a013c99f5d762b600a6ac99c41fbec8b
Pavel <pavel.tsukanov@gmail.com>
2026-05-11 21:26:12 +0300

    Проект перенесен на Spring

---
+++ src/main/java/org/skypro/skyshop/model/article/Article.java
@@ -0,0 +1,59 @@
+package org.skypro.skyshop.model.article;
+
+import org.skypro.skyshop.model.search.Searchable;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public final class Article implements Searchable {
+    private final String articleTitle;
+    private final String articleText;
+
+    private final UUID id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        // Сравниваем только по названию статьи
+        return Objects.equals(articleTitle, article.articleTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(articleTitle);
+    }
+
+    public Article(String articleTitle, String articleText, UUID id) {
+        this.articleTitle = articleTitle;
+        this.articleText = articleText;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Название статьи: " + articleTitle + "\n" +
+                "Текст статьи: " + articleText;
+    }
+
+    @Override
+    public String getSearchTerm() {
+        return this.toString();
+    }
+
+    @Override
+    public String getTypeContent() {
+        return "ARTICLE";
+    }
+
+    @Override
+    public String getName() {
+        return articleTitle;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+}
